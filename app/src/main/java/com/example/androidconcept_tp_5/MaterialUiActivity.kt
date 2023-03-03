93257d89303b84f524fe40deafc40d1655c00022
admin <siddhantpatel445@gmail.com>
2023-03-03 21:41:17 +0530

    menu/intent

--- app/src/main/java/com/example/androidconcept_tp_5/MaterialUiActivity.kt
+++ app/src/main/java/com/example/androidconcept_tp_5/MaterialUiActivity.kt
@@ -1,10 +1,16 @@
 package com.example.androidconcept_tp_5
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
 import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.PopupMenu
 import android.widget.Toast
+import androidx.appcompat.view.menu.MenuBuilder
 import com.example.androidconcept_tp_5.databinding.ActivityMaterialUiBinding
 
 class MaterialUiActivity : AppCompatActivity(), View.OnClickListener {
@@ -13,6 +19,7 @@ class MaterialUiActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         binding = ActivityMaterialUiBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolBar)
 
 //        if (binding.tilLname.editText!!.text.toString().length == 20){
 //            binding.tilLname.error = "YOu have reached.."
@@ -22,14 +29,90 @@ class MaterialUiActivity : AppCompatActivity(), View.OnClickListener {
 
         binding.btnSubmit.setOnClickListener(this)
 
+//        registerForContextMenu(binding.btnSubmit)
+
     }
 
-    override fun onClick(p0: View?) {
-        Toast.makeText(this, binding.tilLname.editText!!.text.toString(), Toast.LENGTH_SHORT).show()
+    override fun onClick(view: View?) {
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("fName", binding.tilFname.editText!!.text.toString())
+            intent.putExtra("lName", binding.tilLname.editText!!.text.toString())
+            intent.putExtra("mobNo", binding.tilMobno.editText!!.text.toString())
+            startActivity(intent)
+
+
+
+
+
+
+
+
+
+//        val popupMenu = PopupMenu(this, view)
+//        menuInflater.inflate(R.menu.options_menu, popupMenu.menu)
+//        popupMenu.show()
+//
+//        popupMenu.setOnMenuItemClickListener(object: PopupMenu.OnMenuItemClickListener{
+//            override fun onMenuItemClick(item: MenuItem?): Boolean {
+//                when(item?.itemId){
+//                    R.id.item_1 ->{
+//                        Toast.makeText(this@MaterialUiActivity, item.title, Toast.LENGTH_SHORT).show()
+//                    }
+//
+//                    R.id.item_2 ->{
+//                        Toast.makeText(this@MaterialUiActivity, item.title, Toast.LENGTH_SHORT).show()
+//                    }
+//                }
+//                return true
+//            }
+//
+//
+//        })
+//        Toast.makeText(this, binding.tilLname.editText!!.text.toString(), Toast.LENGTH_SHORT).show()
     }
 
+    @SuppressLint("RestrictedApi")
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.options_menu, menu)
+
+        val menuBuilder = menu as MenuBuilder
+        menuBuilder.setOptionalIconsVisible(true)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.item_1 ->{
+                Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.item_2 ->{
+                Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.options_menu, menu)
+    }
+
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.item_1 ->{
+                Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.item_2 ->{
+                Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
+            }
+        }
+        return super.onContextItemSelected(item)
     }
 }
--- app/src/main/java/com/example/androidconcept_tp_5/SecondActivity.kt
+++ app/src/main/java/com/example/androidconcept_tp_5/SecondActivity.kt
@@ -2,10 +2,18 @@ package com.example.androidconcept_tp_5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val intent = intent
+        val fName = intent.getStringExtra("fName")
+        val lName = intent.getStringExtra("lName")
+        val mobNo = intent.getStringExtra("mobNo")
+
+        Log.d("Intent_DATA", "Name - $fName $lName \nMobNo - $mobNo")
     }
 }
